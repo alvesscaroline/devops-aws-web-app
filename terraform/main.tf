00b4ea2ba3723a1b95d6e55b7f51344828b05fec
alvesscaroline <carolinesalgadoalves@gmail.com>
2025-03-18 15:48:53 -0300

    refactor(terraform): remove explicit module references in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,9 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   required_version = ">= 1.2.8"
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-module "security_group" {
-  source = "./security-group.tf"
-}
-
-module "ec2" {
-  source = "./ec2.tf"
-}
-
-module "s3" {
-  source = "./s3.tf"
-}
-
-module "cloudwatch" {
-  source = "./cloudwatch.tf"
-}
-
-module "outputs" {
-  source = "./outputs.tf"
 }
